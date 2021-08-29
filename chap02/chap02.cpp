b1526fc7b540341e2b30a4d23e5243954456671f
miracle.huang <miraclemore.huang@gmail.com>
2021-08-29 21:46:29 +0800

    Init commit for learning cpp

---
+++ chap02/chap02.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Chuck Huang on 2021/8/29.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello Chap02!" << endl;
+    return 0;
+}
---
+++ chap03/chap03.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Chuck Huang on 2021/8/29.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello Chap03!" << endl;
+    return 0;
+}
